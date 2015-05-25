0d17fcffd0787abf972a20153c8f86cb191f7661
Vert Reactor <r@vert.bg>
2015-05-25 20:03:54 +0300

    Make use of HAML in layouts of Comfy
    Adding a method "page_haml(tag, index) to be able to use HAML in layouts .

--- lib/comfortable_mexican_sofa/form_builder.rb
+++ lib/comfortable_mexican_sofa/form_builder.rb
@@ -83,6 +83,10 @@ class ComfortableMexicanSofa::FormBuilder < BootstrapForm::FormBuilder
   def page_rich_text(tag, index)
     default_tag_field(tag, index, :text_area_tag, :data => {'cms-rich-text' => true})
   end
+  
+  def page_haml(tag, index)
+     default_tag_field(tag, index, :text_area_tag, :data => {'cms-cm-mode' => 'text/x-haml'})
+  end
 
   def page_file(tag, index)
     default_tag_field(tag, index, :file_field_tag)
